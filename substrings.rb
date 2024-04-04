9c461be776d65cf143fb0ed410913f732ff8ce56
Lance <dogmandevices@gmail.com>
2024-04-04 17:05:08 -0500

    replace count as an accumulator with string.scan.length

--- substrings.rb
+++ substrings.rb
@@ -1,28 +1,14 @@
 def substrings(word, dict)
 	word = word.downcase
-	#cycle through dict to find matches
-	#keep count of each match
-	#for each element of the dictionary,
-	#check if word is equal
-	#if so, pass that word to an array
-	#if not, skip it
-	#count how many times the word is in the array
-	#create a hash with the word as the key and number of
-	#occurances as the value
-	matches = []
 	result = {}
 	dict.each {|key| (
-		count = 0
-		if key == word then
-			count += 1
-			matches.push(key)
+		count = word.scan(key).length
+		if count > 0
 			result[key] = count
-		else
-			count = 0
 		end)}
 	result
 end
 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
